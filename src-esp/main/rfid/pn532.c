1e0832eb0124358dbe30c15449e6702bb3df6b0f
anhad <anhadrs@gmail.com>
2024-05-23 03:02:06 +1000

    We take those i guess

--- src-esp/main/rfid/pn532.c
+++ src-esp/main/rfid/pn532.c
@@ -27,34 +27,21 @@ static void rfid_task(void *arg)
 
     while (1)
     {
-        // pn532_err_t err = pn532_lasterr(pn532);
-        // if (err != PN532_ERR_OK)
-        // {
-        //     // Doing this doesnt seem to help, reset is more effective and doable without changing the lib
-        //     // pn532->lasterr = PN532_ERR_OK;
-        //     pn532 = pn532_init(1, 4, 11, 12, 0x00);
+        int gpio_test = pn532_read_GPIO(pn532);
 
-        //     // We should reset the device here, i.e clean and reinit
-
-        //     continue;
-        // }
-        int cards = pn532_Cards(pn532);
-        // If the result is negative, this means that there was an error, run it through  err to name and reinit
-        if (cards < 0)
+        if (gpio_test < 0)
         {
-            // int delay = 1000 * retries;
-            // ESP_LOGE(TAG, "PN532 error: %s, code: %d, %dth time restarting connection, waiting %dms before restarting...", pn532_err_to_name(-cards), cards, retries, delay);
-            ESP_LOGE(TAG, "PN532 error: %s, code: %d, %dth time restarting connection, restarting...", pn532_err_to_name(-cards), cards, retries);
+            ESP_LOGE(TAG, "PN532 error: %s, code: %d, %dth time restarting connection, restarting...", pn532_err_to_name(-gpio_test), gpio_test, retries);
             pn532_end(pn532);
+            vTaskDelay(pdMS_TO_TICKS(100));
             pn532 = pn532_init(UART_PORT, BAUD_SETTING, TX_UART_PIN, RX_UART_PIN, 0x00);
             // Apply exponential backoff to the initialisation, and the amoutn of time you wait between then
-            vTaskDelay(pdMS_TO_TICKS(10000));
             while (!pn532)
             {
                 ESP_LOGE(TAG, "Failed to init again trying again");
                 pn532_end(pn532);
-                pn532 = pn532_init(UART_PORT, BAUD_SETTING, TX_UART_PIN, RX_UART_PIN, 0x00);
                 vTaskDelay(pdMS_TO_TICKS(100));
+                pn532 = pn532_init(UART_PORT, BAUD_SETTING, TX_UART_PIN, RX_UART_PIN, 0x00);
 
                 retries++;
                 if (retries > MAX_RETRIES)
@@ -70,6 +57,17 @@ static void rfid_task(void *arg)
             }
 
             continue;
+        }
+        int cards = pn532_Cards(pn532);
+        // If the result is negative, this means that there was an error, run it through  err to name and reinit
+        if (cards < 0)
+        {
+            ESP_LOGE(TAG, "Reading cards PN532 error: %s, code: %d", pn532_err_to_name(-cards), cards);
+        }
+
+        if (cards >= 0)
+        {
+            retries = 0;
         }
 
         if (cards > 0)
@@ -114,10 +112,11 @@ static void rfid_task(void *arg)
                 break;
             }
         }
-        // vTaskDelay(pdMS_TO_TICKS(100));
-        taskYIELD();
+        vTaskDelay(pdMS_TO_TICKS(100));
+        // taskYIELD();
     }
     ESP_LOGI(TAG, "Task finished");
+    esp_restart();
 
     vTaskDelete(NULL);
 }
